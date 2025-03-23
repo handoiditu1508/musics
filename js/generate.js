20d7c7653180e4f6346096834c4c10cebcc03f9e
Danh <handoiditu1508@gmail.com>
2025-03-23 22:01:59 +0700

    generate json file

--- js/generate.js
+++ js/generate.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const files = fs.readdirSync("../musics");
 
 const writeStream = fs.createWriteStream("filesList.js", { flags: "w" });
-writeStream.write("const filesList=[")
+writeStream.write("const filesList=[");
 files.forEach((file, index) => {
   if (index === files.length - 1) {
     writeStream.write(`"musics/${file}"`);
@@ -12,3 +12,7 @@ files.forEach((file, index) => {
   }
 });
 writeStream.end("];");
+
+const jsonWriteStream = fs.createWriteStream("filesList.json", { flags: "w" });
+jsonWriteStream.write(JSON.stringify(files));
+jsonWriteStream.end();
