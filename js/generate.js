f60fb282bfc4f04f83680b0752ae9554488f3938
Danh <handoiditu1508@gmail.com>
2025-03-26 00:32:14 +0700

    generate more complex json file

--- js/generate.js
+++ js/generate.js
@@ -1,18 +1,49 @@
 const fs = require('fs');
+const mm = require('music-metadata');
+const path = require('path');
 
 const files = fs.readdirSync("../musics");
 
+// store songs's information to write to filesList.json
+const songInfos = [];
+const tasks = [];
+
+// write to filesList.js
 const writeStream = fs.createWriteStream("filesList.js", { flags: "w" });
 writeStream.write("const filesList=[");
+
 files.forEach((file, index) => {
+  // write to filesList.js
   if (index === files.length - 1) {
     writeStream.write(`"musics/${file}"`);
   } else {
     writeStream.write(`"musics/${file}",`);
   }
+
+  // get song's information
+  const filePath = path.join("../musics", file);
+  const fileStats = fs.statSync(filePath);
+  const task = mm.parseFile(filePath, { duration: true });
+  tasks.push(task);
+  task.then((metadata) => {
+    songInfos.push({
+      id: index + 1,
+      name: file,
+      title: metadata.common.title || "",
+      artists: metadata.common.artists || [],
+      size: fileStats.size,
+      duration: metadata.format.duration,
+    });
+  });
 });
+
+// write to filesList.js
 writeStream.end("];");
 
-const jsonWriteStream = fs.createWriteStream("filesList.json", { flags: "w" });
-jsonWriteStream.write(JSON.stringify(files));
-jsonWriteStream.end();
+// write to filesList.json
+Promise.all(tasks).then(() => {
+  songInfos.sort((a, b) => a.id - b.id);
+  const jsonWriteStream = fs.createWriteStream("filesList.json", { flags: "w" });
+  jsonWriteStream.write(JSON.stringify(songInfos));
+  jsonWriteStream.end();
+});
